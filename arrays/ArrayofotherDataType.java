3e46a79dc84e0354f8a5823efe927ed550fe435e
khushisingh-22 <ks6637890@gmail.com>
2026-02-23 18:44:09 +0530

    Added new file inside arrays folder

---
+++ arrays/ArrayofotherDataType.java
@@ -0,0 +1,33 @@
+// public class ArrayofotherDataType{
+//      public static void main(String[] args) {
+//       // by defaulting value of double 
+//        double[] arr = new double[7];
+//        for( int i=0; i<arr.length; i++){
+//           System.out.println(arr[i]);
+//        }
+//      }
+// }
+
+// 
+// we make a array of string &char  also 
+
+public class ArrayofotherDataType{
+     public static void main(String[] args) {
+             char[] arr = new char[7];
+       for( int i=0; i<arr.length; i++){
+          System.out.println(arr[i]);
+       }
+     }
+}
+
+
+public class ArrayofotherDataType{
+     public static void main(String[] args) {
+             String[] arr = {khushi,singh,sakshi};
+       for( int i=0; i<arr.length; i++){
+          System.out.println(arr[i]);
+       }
+     }
+}
+
+
