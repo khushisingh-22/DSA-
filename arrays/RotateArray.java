9762170cedb1856312ff983e2946d3d8b6888305
khushisingh-22 <ks6637890@gmail.com>
2026-02-24 23:50:24 +0530

    fix error

---
+++ arrays/RotateArray.java
@@ -0,0 +1,31 @@
+ublic class RotateArray {
+    public static void main(String[] args) {
+
+
+        int[] arr = {1, 2, 3, 4, 5};
+        int r = 2;
+        int n = arr.length;
+
+        r = r % n;   // safety
+
+        // rotate r times
+        for (int i = 0; i < r; i++) {
+            int last = arr[n - 1];
+
+            for (int j = n - 1; j > 0; j--) {
+                arr[j] = arr[j - 1];
+            }
+
+            arr[0] = last;
+        }
+
+        // print rotated array
+        for (int ele : arr) {
+            System.out.print(ele + " ");
+        }
+    }
+}
+        
+    
+    
+    
