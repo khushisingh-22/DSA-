fd3eeef5ade341565e4d79f9bba113a9d5258700
khushisingh-22 <ks6637890@gmail.com>
2026-02-24 18:51:29 +0530

    added a question

--- arrays/Twosum.java
+++ arrays/Twosum.java
@@ -17,3 +17,26 @@ public class TWOSUM {
         System.out.println("No pair found");
     }
 }
+
+//ques:print the second  maximum element in the array 
+
+public class TWOSUM {
+    public static void main(String[] args) {
+
+       int arr[] ={2,4,6,10};
+       int max = Integer.MIN_VALUE;
+       int smax = Integer.MIN_VALUE;
+       //calculate max value 
+       for (i=0; i<arr.length; i++){
+        if(arr[i]>max) max=arr[i];
+       }
+           // calculate second largest 
+       for (i=0; i<arr.length; i++){
+        if(arr[i]>smax && arr[i]!=max) smax=arr[i];
+       }
+       System.out.println(max);
+       System.out.println(smax);
+    }
+    }
+    
+
