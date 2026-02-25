88cf0ee830e3b57dd55542a732b3135ec9ce90b9
khushisingh-22 <ks6637890@gmail.com>
2026-02-25 18:42:49 +0530

    WavyArray

---
+++ arrays/WavyArray.java
@@ -0,0 +1,21 @@
+public class WavyArray {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5, 6};
+
+        // wavy banana
+        for (int i = 0; i < arr.length - 1; i = i + 2) {
+
+            // swap arr[i] and arr[i+1]
+            int temp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temp;
+        }
+
+        // print array
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
