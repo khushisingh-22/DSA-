8cb6665fa1a4e6492e0a6664ae056303030426c5
khushisingh-22 <ks6637890@gmail.com>
2026-03-02 19:00:39 +0530

    update file

---
+++ arrays/Arraylist.java
@@ -0,0 +1,14 @@
+
+ import java.util.ArrayList;
+public class Arraylist {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(25);
+        arr.add(8);
+        arr.add(7);
+        arr.add(8);
+        System.out.println(arr.get(3));
+    
+
+    }
+}
--- arrays/WavyArray.java
+++ arrays/WavyArray.java
@@ -16,6 +16,8 @@ public class WavyArray {
         // print array
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
+
         }
     }
+
 }
